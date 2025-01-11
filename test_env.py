c681dd84386ce370c9d600b3b96697bc7f0fff4b
adir-jscode <adir.jscode@gmail.com>
2025-01-11 07:07:31 +0600

    Feature Engineering Added

---
+++ test_env.py
@@ -0,0 +1,16 @@
+import sys
+import tensorflow as tf
+import pandas as pd
+import sklearn as sk
+import numpy as np
+import matplotlib
+
+print(f"Python {sys.version}")
+print(f"TensorFlow version: {tf.__version__}")
+print(f"Keras Version: {tf.keras.__version__}")
+print(f"Pandas: {pd.__version__}")
+print(f"Scikit-Learn: {sk.__version__}")
+print(f"Numpy: {np.__version__}")
+print(f"Matplotlib: {matplotlib.__version__}")
+print(f"GPU is {'available' if tf.config.list_physical_devices('GPU') else 'not available'}")
+print(f"CUDA: {tf.test.is_built_with_cuda()}")
